5137c452ef3d2653ac335f866a759865f29d83bb
Phani Kumar <Phani.Sikakollu@ADP.com>
2019-05-08 12:19:51 +0530

    Edit deductions
    Edit Deductions intial checkin

--- src/app/deductions/deductions.component.ts
+++ src/app/deductions/deductions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-deductions',
@@ -7,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeductionsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router, private  route: ActivatedRoute) { }
 
+  
+  userMessage : string;
+  deductionsAmount : number;
+  edited =false;
+
+  
   ngOnInit() {
+   this.edited=false;
+   this.deductionsAmount=30;
   }
 
-  deductionsAmount : number;
-  deductionsGoal : number;
+  onEditDeductions(){
+    
+    this.edited = true;
+  }
+
 
 }
